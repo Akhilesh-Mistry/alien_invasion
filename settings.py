403c348fae9698fc9d7cb130b4a8c403ea1657e4
A Mistry <apmistry29@hotmail.com>
2020-09-05 20:11:03 +0100

    Moving ship established

--- settings.py
+++ settings.py
@@ -6,4 +6,7 @@ class Settings:
         # Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_colour = (200,100,21)
+        self.bg_colour = (10,0,25)
+
+        # Ship settings
+        self.ship_speed = 1.5
